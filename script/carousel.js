ca56e79dc4d8ae112116d93303efd57f9eff3ac0
hui425524 <amy098534562@gmail.com>
2025-08-29 11:57:33 +0800

    0829

--- script/carousel.js
+++ script/carousel.js
@@ -1,6 +1,6 @@
- 
- 
- function bannerSwitcher() {
+///// Section-1 CSS-Slider /////    
+  // Auto Switching Images for CSS-Slider
+  function bannerSwitcher() {
     next = $('.sec-1-input').filter(':checked').next('.sec-1-input');
     if (next.length) next.prop('checked', true);
     else $('.sec-1-input').first().prop('checked', true);
